c11dad5eab6a50adc82f061d50ce1e5fabbaf584
OptimusJc <optimus.prime.jc@gmail.com>
2021-07-09 13:45:20 +0300

    Added functionality for the content page

---
+++ assets/js/data.js
@@ -0,0 +1,65 @@
+const data = [
+    {
+        "pic": "/assets/images/buffalo.jpg",
+        "name": "elephant art",
+        "description": "this is a simple artwork of an elephant",
+        "id": 1
+    },
+    {
+        "pic": "/assets/images/cobra.jpg",
+        "name": "cobra art",
+        "description": "this is a simple artwork of a cobra",
+        "id": 2
+    },
+    {
+        "pic": "/assets/images/elephant 2.jpg",
+        "name": "elephant 2 art",
+        "description": "this is a simple artwork of an elephant kenya",
+        "id": 3
+    },
+    {
+        "pic": "/assets/images/girrafe.jpg",
+        "name": "girrafe art",
+        "description": "this is a simple artwork of an girrafe",
+        "id": 4
+    },
+    {
+        "pic": "/assets/images/hippo 2.jpg",
+        "name": "hippo art",
+        "description": "this is a simple artwork of a hippo",
+        "id": 5
+    },
+    {
+        "pic": "/assets/images/lizzard.jpg",
+        "name": "lizzard art",
+        "description": "this is a simple artwork of a lizzard",
+        "id": 6
+    },
+    {
+        "pic": "/assets/images/ostrich.jpg",
+        "name": "ostrich art",
+        "description": "this is a simple artwork of an ostrich",
+        "id": 7
+    },
+    {
+        "pic": "/assets/images/rihno.jpg",
+        "name": "rihno art",
+        "description": "this is a simple artwork of an rihno",
+        "id": 8
+    },
+    {
+        "pic": "/assets/images/wilde beast.jpg",
+        "name": "wilde beast art",
+        "description": "this is a simple artwork of an wilde beast",
+        "id": 9
+    },
+    {
+        "pic": "/assets/images/zebra.jpg",
+        "name": "zebra art",
+        "description": "this is a simple artwork of an zebra",
+        "id": 10
+    }
+];
+
+
+export default data;
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,73 @@
-$(document).ready(() => {
-    const menu_icon = $('img').filter('.menu');
-    const nav = $('nav');
+import data  from './data.js';
+
+window.addEventListener('load', () => {
     
-    menu_icon.click((e) => {
-        if(nav.css('left') == 0) {
+    // create elements    
+    const media = document.createElement('div');
+    const media_item = document.createElement('div');
+    const media_body = document.createElement('div');
+    const img = document.createElement('img');
+    const h3 = document.createElement('h3');
+    const p = document.createElement('p');
+    
+    const element = data[0];
+    
+    const container = document.querySelector('.container');
+    
+    // attach elements 
+    container.appendChild(media);
+    media.appendChild(media_item);
+    media_item.appendChild(img);
+    media.appendChild(media_body);
+    media_body.appendChild(h3);
+    media_body.appendChild(p);
+    
+    // col.classList.add('container');
+    media.classList.add('media');
+    
+    img.setAttribute('src', `${element.pic}`);
+    media_item.classList.add('media-item');
+    
+    media_body.classList.add("media-body");
+    h3.innerHTML = element.name;
+    h3.classList.add('media-head');
+    
+    p.innerHTML = element.description;
 
-            e.target.src = '/assets/images/icons/menu.svg';
-            nav.css('left', '-110%');
-        } else {
-            
-            e.target.src = '/assets/images/icons/close.svg';
-            nav.css('left', 0);
-        }
-    })
+});  
 
-})
+
+// const content = (element) => {
+
+//         // create elements    
+//         const media = document.createElement('div');
+//         const media_item = document.createElement('div');
+//         const media_body = document.createElement('div');
+//         const img = document.createElement('img');
+//         const h3 = document.createElement('h3');
+//         const p = document.createElement('p');
+        
+//         const element = data[0];
+        
+//         const container = document.querySelector('.container');
+        
+//         // attach elements 
+//         container.appendChild(media);
+//         media.appendChild(media_item);
+//         media_item.appendChild(img);
+//         media.appendChild(media_body);
+//         media_body.appendChild(h3);
+//         media_body.appendChild(p);
+        
+//         // col.classList.add('container');
+//         media.classList.add('media');
+        
+//         img.setAttribute('src', `${element.pic}`);
+//         media_item.classList.add('media-item');
+        
+//         media_body.classList.add("media-body");
+//         h3.innerHTML = element.name;
+//         h3.classList.add('media-head');
+        
+//         p.innerHTML = element.description;
+// }
